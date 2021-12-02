5214beda830eef98a06d60066df5a3b68157139c
rasyidcode <jamil@dwebsite.net>
2021-12-02 08:44:12 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: kPrimaryColor,
       ),
-      home: AbsenPage(),
+      home: LoginPage(),
     );
   }
 }
